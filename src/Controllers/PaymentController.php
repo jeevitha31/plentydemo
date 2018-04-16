5c6e4e7c328b82abbb75b4542e33a8ada86c5fa7
jeevitha31 <38306170+jeevitha31@users.noreply.github.com>
2018-04-16 17:58:03 +0530

    Add files via upload

--- src/Controllers/PaymentController.php
+++ src/Controllers/PaymentController.php
@@ -111,7 +111,10 @@ class PaymentController extends Controller
     {
         $requestData = $this->request->all();
         $this->getLogger(__METHOD__)->error('ExecutePayment response.', $requestData);
-     
+        if(!empty($requestData['cp_checkout_token']))
+        {
+         $this->sessionStorage->getPlugin()->setValue('barzahlen',$token);
+		}
         $requestData['payment_id'] = (!empty($requestData['payment_id'])) ? $requestData['payment_id'] : $requestData['key'];
 
 		$this->getLogger(__METHOD__)->error('ExecutePayment response1.', $requestData['payment_id']);
