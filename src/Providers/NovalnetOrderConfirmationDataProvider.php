7aa5c37451d106ac8dce8e0fe938158505760c18
jeevitha31 <38306170+jeevitha31@users.noreply.github.com>
2018-04-17 17:23:42 +0530

    Update NovalnetOrderConfirmationDataProvider.php

--- src/Providers/NovalnetOrderConfirmationDataProvider.php
+++ src/Providers/NovalnetOrderConfirmationDataProvider.php
@@ -70,7 +70,7 @@ class NovalnetOrderConfirmationDataProvider
                 {
                     $comment .= (string)$data->text;
                     $comment .= '</br>';
-                    $comment .= '<script> var note='heloo';alert(note);</script>';
+                    $comment .= '<script> var note="heloo";alert(note);</script>';
                 }
 		     
 
