931077025fc7860cde0f97c8a7971acec9a8b1cd
jeevitha31 <38306170+jeevitha31@users.noreply.github.com>
2018-04-17 11:38:10 +0530

    Add files via upload

--- src/Helper/PaymentHelper.php
+++ src/Helper/PaymentHelper.php
@@ -209,6 +209,11 @@ class PaymentHelper
         $paymentProperty[]   = $this->getPaymentProperty(PaymentProperty::TYPE_BOOKING_TEXT, $transactionId);
         $paymentProperty[]   = $this->getPaymentProperty(PaymentProperty::TYPE_TRANSACTION_ID, $transactionId);
         $paymentProperty[]   = $this->getPaymentProperty(PaymentProperty::TYPE_ORIGIN, Payment::ORIGIN_PLUGIN);
+        if($requestData['payment_type'] == 'CASHPAYMENT' && !empty($requestData['cp_checkout_token']))
+        {
+			$paymentProperty[]   = $this->getPaymentProperty(PaymentProperty::TYPE_PAYMENT_TEXT,$requestData['cp_checkout_token']);
+			 $this->getLogger(__METHOD__)->error('Barzhalen paymet token', $requestData['cp_checkout_token']);
+		}
         $payment->properties = $paymentProperty;
 
         $paymentObj = $this->paymentRepository->createPayment($payment);
